eabb72c2cf34227401a22ec3b1e69e97c9c851a3
soonraah <soonraah.dev@gmail.com>
2019-11-05 00:38:31 +0900

    [#23] Add setup info

--- setup.py
+++ setup.py
@@ -1,2 +1,15 @@
 from setuptools import setup
-setup()
+
+setup(
+    name='evmoon',
+    version='0.0.1',
+    install_requires=[
+        'Sphinx',
+        'matplotlib',
+        'pandas',
+        'numpy',
+        'scipy'
+    ],
+    url='https://github.com/soonraah/evening_moon',
+    description='Get and analyze data of financial instruments provided by SBI Securities'
+)
