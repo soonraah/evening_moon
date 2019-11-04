1369eb8b1c94d2ab9ae0ddd3246bdd99bfbf0e46
soonraah <soonraah.dev@gmail.com>
2019-11-05 01:34:27 +0900

    [#22] Change arguments of show_mean_std_diagram()

--- evmoon/chart.py
+++ evmoon/chart.py
@@ -25,17 +25,24 @@ def show_rate_of_return_chart(fund_codes: list,
     ax.set_ylabel("Rate of Return")
 
 
-def show_mean_std_diagram(fund_codes: list,
-                          start_period: datetime.date = None,
-                          end_period: datetime.date = None,
-                          investment_period_days: int = 5,
-                          num_random_feasible_set: int = 0) -> None:
+def show_mean_std_diagram_with_time_series(fund_codes: list,
+                                           start_period: datetime.date = None,
+                                           end_period: datetime.date = None,
+                                           investment_period_days: int = 5,
+                                           num_random_feasible_set: int = 0) -> None:
     df_return = calc_rate_of_return(fund_codes, start_period, end_period, investment_period_days)
     matrix = df_return.as_matrix()
 
     mean = matrix.mean(axis=0)
-    std = matrix.std(axis=0, ddof=0)
+    cov = np.cov(matrix, rowvar=False, ddof=0)
 
+    show_mean_std_diagram(fund_codes, mean, cov, num_random_feasible_set)
+
+
+def show_mean_std_diagram(fund_codes: list,
+                          mean: np.ndarray,
+                          cov: np.ndarray,
+                          num_random_feasible_set: int = 0) -> None:
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
@@ -43,12 +50,17 @@ def show_mean_std_diagram(fund_codes: list,
 
     # ランダムな実現可能集合の要素数が正の場合はそれらもポートフォリオとして表示する
     if num_random_feasible_set > 0:
-        cov = np.cov(matrix, rowvar=False, ddof=0)
         portfolio_mean_std_weight = calc_random_weight_portfolios(num_random_feasible_set, mean, cov)
         ax.scatter(x=portfolio_mean_std_weight[1], y=portfolio_mean_std_weight[0], c='lightskyblue', s=5, marker='o',
                    label='random feasible set')
 
-    ax.scatter(x=std, y=mean, c='navy', s=16, marker='x', label='fund')
+    # 各ファンドをプロット
+    stddev = np.power(cov.diagonal(), 2)
+    ax.scatter(x=stddev, y=mean, c='navy', s=16, marker='x', label='fund')
+
+    for i, fund_code in enumerate(fund_codes):
+        ax.annotate(fund_code, stddev[i], mean[i])
+
     ax.legend()
 
     plt.xlabel('std')
